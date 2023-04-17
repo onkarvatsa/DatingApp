931336d2754a9c3085210e6967ca7e67161254bf
Onkar Sharma <osharma@epaysystems.com>
2023-04-18 00:59:48 +0530

    End of section 3: By Vatsa

--- API/Program.cs
+++ API/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // // builder.Services.AddEndpointsApiExplorer();
 // // builder.Services.AddSwaggerGen();
@@ -18,6 +20,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 // // if (app.Environment.IsDevelopment())
 // // {

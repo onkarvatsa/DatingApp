774ddcea6b4f409c3735aeb162c8d91e2a6863b3
Onkar Sharma <osharma@epaysystems.com>
2023-04-24 22:33:43 +0530

    Committed by Onkar Sharma -- Working

--- API/Program.cs
+++ API/Program.cs
@@ -13,27 +13,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
 
 builder.Services.AddCors();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-// // builder.Services.AddEndpointsApiExplorer();
-// // builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
-// // if (app.Environment.IsDevelopment())
-// // {
-// //     app.UseSwagger();
-// //     app.UseSwaggerUI();
-// // }
-
-// // app.UseHttpsRedirection();
-
-// // app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
 
-//Onkar Sharma
